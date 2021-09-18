233fc0790a99538684a0a96beb9999156829f0fa
Liza Volkova <elvolkov@estee.com>
2021-09-18 16:56:47 -0400

    sets siteSettings to default

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { useSiteContext } from '../context/use-site';
 
 export default function Header() {
-    const siteSettings = useSiteContext();
+    const { siteSettings = {} } = useSiteContext();
 
     return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
