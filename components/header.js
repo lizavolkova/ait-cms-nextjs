2756fd4ae625110d1279ca0d302e6f0c66307902
Liza Volkova <elvolkov@estee.com>
2021-09-18 16:52:36 -0400

    replaces ternary with optional chaining

--- components/header.js
+++ components/header.js
@@ -3,13 +3,11 @@ import { useSiteContext } from '../context/use-site';
 
 export default function Header() {
     const siteSettings = useSiteContext();
-    const { generalSettings } = siteSettings ? siteSettings : {};
-    const { title } = generalSettings ? generalSettings : {};
 
     return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
-        <a className="hover:underline">{title}</a>
+        <a className="hover:underline">{siteSettings?.generalSettings?.title}</a>
       </Link>
     </h2>
     )
