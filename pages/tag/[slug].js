10509b9ed25734ed5674fbea5aa944ade6d4b613
Liza Volkova <elvolkov@estee.com>
2021-09-21 10:49:32 -0400

    adds updated tag page

---
+++ pages/tag/[slug].js
@@ -0,0 +1,40 @@
+import {
+  getBlogSettings,
+  getAllTags,
+  getAllPostsByTag
+} from '../../lib/api'
+import AllPosts from "../../components/layout/all-posts";
+import Container from "../../components/container";
+import Header from "../../components/layout/header";
+import Layout from "../../components/layout/layout";
+
+export default function Post({ allPosts = {}, preview, settings, params }) {
+  const { edges } = allPosts;
+
+  return (
+      <Layout preview={preview}>
+        <Container>
+          <Header />
+          <div>tag: {params?.slug}</div>
+          {edges && <AllPosts morePosts={edges}/>}
+        </Container>
+      </Layout>
+  )
+}
+export async function getStaticProps({ params, preview = false, previewData }) {
+  const settings = await getBlogSettings();
+  const allPosts = await getAllPostsByTag(preview, params.slug)
+
+  return {
+    props: { allPosts, preview, settings, params }
+  }
+}
+
+export async function getStaticPaths() {
+  const allTags = await getAllTags();
+
+  return {
+    paths: allTags.edges.map(({ node }) => `/tag/${node.slug}`) || [],
+    fallback: true,
+  }
+}
